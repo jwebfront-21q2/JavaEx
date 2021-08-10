c8dd49b01d61715439ae6eb39c299b9b12199107
남승균 <skyun.nam@gmail.com>
2021-08-10 11:48:26 +0900

    DAO UPDATE, DELETE, Search

--- JavaEx/src/com/javaex/jdbc/oracle/dao/DAOApp.java
+++ JavaEx/src/com/javaex/jdbc/oracle/dao/DAOApp.java
@@ -7,11 +7,68 @@ import java.util.Scanner;
 public class DAOApp {
 
 	public static void main(String[] args) {
-		listAuthors();
+//		listAuthors();
 		
-		insertAuthor();
+//		insertAuthor();
+//		updateAuthor();
+//		deleteAuthor();
+		searchAuthor();
 		
-		listAuthors();
+//		listAuthors();
+	}
+	
+	private static void searchAuthor() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("검색어:");
+		String keyword = scanner.next();
+		
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		
+		List<AuthorVO> list = dao.search(keyword);
+		Iterator<AuthorVO> it = list.iterator();
+		
+		while(it.hasNext()) {
+			AuthorVO vo = it.next();
+			System.out.printf("%d\t%s\t%s%n",
+					vo.getAuthorId(),
+					vo.getAuthorName(),
+					vo.getAuthorDesc());
+		}
+		scanner.close();
+		
+	}
+	
+	private static void deleteAuthor() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("저자번호:");
+		long authorId = scanner.nextLong();
+		
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		
+		boolean success = dao.delete(authorId);
+		
+		System.out.println("Author DELETE:" + (success ? "성공": "실패"));
+		scanner.close();
+	}
+	
+	private static void updateAuthor() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("저자번호:");
+		long authorId = scanner.nextLong();
+		System.out.print("이름:");
+		String name = scanner.next();
+		System.out.print("경력:");
+		String desc = scanner.next();
+		
+		AuthorVO vo = new AuthorVO(authorId, name, desc);
+		
+		AuthorDAO dao = new AuthorDAOImplOracle();
+		
+		boolean success = dao.update(vo);
+		
+		System.out.println("Author UPDATE:" + (success ? "성공": "실패"));
+		scanner.close();
 	}
 	
 	private static void insertAuthor() {
